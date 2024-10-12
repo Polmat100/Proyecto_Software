6569123565c4598161b0f91d7e149e807f77ebab
Polmat100 <mccmatiasrenato@hotmail.com>
2024-10-12 16:21:37 -0500

    Subiendo mi parte de inicio y registro

--- front-react/src/pages/login/Login.jsx
+++ front-react/src/pages/login/Login.jsx
@@ -1,5 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from 'react-router-dom';
 
 export const Login = () => {
-  return <div>Login</div>;
+  
+
+  return (
+    <div className="container mt-2">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="110"
+              height="110"
+              fill="currentColor"
+              className="bi bi-shop-window mb-4"
+              viewBox="0 0 16 16"
+            >
+              <path d="M2.97 1.35A1 1 0 0 1 3.73 1h8.54a1 1 0 0 1 .76.35l2.609 3.044A1.5 1.5 0 0 1 16 5.37v.255a2.375 2.375 0 0 1-4.25 1.458A2.37 2.37 0 0 1 9.875 8 2.37 2.37 0 0 1 8 7.083 2.37 2.37 0 0 1 6.125 8a2.37 2.37 0 0 1-1.875-.917A2.375 2.375 0 0 1 0 5.625V5.37a1.5 1.5 0 0 1 .361-.976zm1.78 4.275a1.375 1.375 0 0 0 2.75 0 .5.5 0 0 1 1 0 1.375 1.375 0 0 0 2.75 0 .5.5 0 0 1 1 0 1.375 1.375 0 1 0 2.75 0V5.37a.5.5 0 0 0-.12-.325L12.27 2H3.73L1.12 5.045A.5.5 0 0 0 1 5.37v.255a1.375 1.375 0 0 0 2.75 0 .5.5 0 0 1 1 0M1.5 8.5A.5.5 0 0 1 2 9v6h12V9a.5.5 0 0 1 1 0v6h.5a.5.5 0 0 1 0 1H.5a.5.5 0 0 1 0-1H1V9a.5.5 0 0 1 .5-.5m2 .5a.5.5 0 0 1 .5.5V13h8V9.5a.5.5 0 0 1 1 0V13a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V9.5a.5.5 0 0 1 .5-.5" />
+            </svg>
+      <h2 className="text-center mb-4">Bienvenidos a U-Market</h2>
+      
+      <form className="w-50 mx-auto">
+        <div className="mb-3">
+          <label htmlFor="email" className="form-label">Correo electrónico</label>
+          <input
+            type="email"
+            className="form-control"
+            id="email"
+            required
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="password" className="form-label">Contraseña</label>
+          <input
+            type="password"
+            className="form-control mb-2"
+            id="password"
+            required
+          />
+        </div>
+        <div>
+            <Link to="/" className="btn bg-footer-custom pt-2 btn-primary w-100 mb-2">Iniciar Sesión</Link> 
+        </div>
+        <div>
+            <Link to="/Register" className="btn bg-footer-custom pt-2 btn-primary w-100">Registrase</Link> 
+        </div>
+
+       
+      </form>
+    </div>
+  );
 };
